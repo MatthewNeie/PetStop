1883d70b430ea34a79bdeccd8dec95cee7c74ed0
Matthew <Matthew@Kathys-iMac.local>
2023-09-14 19:24:45 -0700

    test

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className='App'>
         <h1>Boilerplate</h1>
         <img id='comp-img' src='./computer.png'></img>
-        <p>Replace the starter code in this template with something cool</p>
+        <p>CAPSTONE</p>
         <Login />
     </div>
   );
