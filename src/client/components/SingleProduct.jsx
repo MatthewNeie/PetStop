1c050923f45e751faec36e7698152939c7ba44f8
Matthew <Matthew@Kathys-iMac.local>
2023-10-11 17:08:07 -0700

    update product

--- src/client/components/SingleProduct.jsx
+++ src/client/components/SingleProduct.jsx
@@ -4,6 +4,7 @@ import { useParams, useNavigate } from 'react-router-dom'
 import { fetchUsersById } from '../api/UsersAjaxHelper';
 import { deleteProduct } from '../api/ProductsAjaxHelper';
 import postReview from '../api/ReviewsAjaxHelper';
+import { updateProduct } from '../api/ProductsAjaxHelper';
 
 const SingleProduct = ({ products , reviews, token, addToCart }) => {
 
@@ -12,6 +13,19 @@ const SingleProduct = ({ products , reviews, token, addToCart }) => {
     const [date, setDate] = useState('')
     const [userId, setUserId] = useState(window.localStorage.getItem("userId"));
     const [adminUser, setAdminUser] = useState({});
+    const [changeProduct, setChangeProduct] = useState('')
+
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [quantity, setQuantity] = useState('');
+    const [petType, setPetType] = useState('');
+    const [productType, setProductType] = useState('');
+    const [inStock, setInStock] = useState('');
+    const [isPopular, setIsPopular] = useState('');
+    const [imgUrl, setImgUrl] = useState('');
+
+    const[stateHandler, setStateHandler] = useState(false)
 
     const { productId } = useParams();
 
@@ -57,6 +71,31 @@ const SingleProduct = ({ products , reviews, token, addToCart }) => {
             }
             getUsersById();
         }, [])
+
+        async function submitUpdateProduct(e) {
+            e.preventDefault();
+    
+                const product = {
+                    product: {
+                        name,
+                        description,
+                        price,
+                        quantity,
+                        petType,
+                        productType,
+                        inStock,
+                        isPopular,
+                        imgUrl,
+                    }
+                };
+    
+            
+                const response = await updateProduct(product, productId, token);
+                console.log(response)
+        
+            }
+
+            
 
     async function submitReview(e) {
         e.preventDefault();
@@ -110,9 +149,215 @@ const SingleProduct = ({ products , reviews, token, addToCart }) => {
 
             {adminUser.isAdministrator ?
             <div>
-                <button className="update-button" >Update</button>
+                <button className="update-button" onClick={() => {setStateHandler(true)}}>Update</button>
                 <button className="delete-button" onClick={deleteProductById} >Delete</button>
             </div> : null }
+
+            {stateHandler === true ?
+            <div>
+                {products.filter(product => product.id === productIdNumberfy).map(filteredProduct => (
+                <form onSubmit={submitUpdateProduct}>
+                <div className="form-div">
+                        <label className="form-label" for="name">Name</label>
+                    <br></br>
+                        <input className="product-name-box"
+                            type="text"
+                            initialValue={filteredProduct.name}
+                            value={name}
+                            onChange={(e) => setName(e.target.value)} id="name"
+                            placeholder="Name" 
+                            required />
+                    </div>
+
+                    <div className="form-div">
+                        <label className="form-label" for="description">Description</label>
+                    <br></br>
+                        <textarea
+                            className="product-desc-box"
+                            rows={4}
+                            cols={40}
+                            initialValue={filteredProduct.description}
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            required
+                                    />
+                    </div>
+
+                    <div className="form-div">
+                        <label className="form-label" for="price">Price</label>
+                    <br></br>
+                        <input type="text" id="price"
+                            className="product-price-box"
+                            initialValue={filteredProduct.price}
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
+                            placeholder="Price"
+                            required />
+                    </div>
+
+                    <div className="form-div">
+                        <label className="form-label">Quantity</label>
+                    <br></br>
+                        <input
+                            className="product-quantity-box"
+                            type="text"
+                            initialValue={filteredProduct.quantity}
+                            value={quantity}
+                            placeholder="Quantity"
+                            onChange={(e) => setQuantity(e.target.value)}
+                            required />
+
+                    </div>
+
+                    <div className="form-div">
+                        <label className="form-label">Pet Type</label>
+                    <br></br>
+                            <input
+                                type="radio"
+                                name="petType"
+                                value='dog'
+                                onChange={(e) => setPetType(e.target.value)}/>
+                                Dog
+                            <input
+                                type="radio"
+                                name="petType"
+                                value='cat'
+                                onChange={(e) => setPetType(e.target.value)}/>
+                                Cat
+                            <input
+                                type="radio"
+                                name="petType"
+                                value='bird'
+                                onChange={(e) => setPetType(e.target.value)}/>
+                                Bird
+                            <input
+                                type="radio"
+                                name="petType"
+                                value='reptile-amphibian'
+                                onChange={(e) => setPetType(e.target.value)}/>
+                                Reptile
+
+                    </div>
+                    <div className="form-div">
+                        <label className="form-label">Product Type</label>
+                    <br></br>
+                        <input
+                                type="radio"
+                                name="productType"
+                                value='food'
+                                onChange={(e) => setProductType(e.target.value)}/>
+                                Food
+                            <input
+                                type="radio"
+                                name="productType"
+                                value='toy'
+                                onChange={(e) => setProductType(e.target.value)}/>
+                                Toy
+                            <input
+                                type="radio"
+                                name="productType"
+                                value='bed'
+                                onChange={(e) => setProductType(e.target.value)}/>
+                                Bed
+                            <input
+                                type="radio"
+                                name="productType"
+                                value='house'
+                                onChange={(e) => setProductType(e.target.value)}/>
+                                House
+                            <input
+                                type="radio"
+                                name="productType"
+                                value='cage'
+                                onChange={(e) => setProductType(e.target.value)}/>
+                                Cage
+                            <input
+                                type="radio"
+                                name="productType"
+                                value='scratcher'
+                                onChange={(e) => setProductType(e.target.value)}/>
+                                Scratcher
+                        <br></br>
+                            <input
+                                type="radio"
+                                name="productType"
+                                value='tree'
+                                onChange={(e) => setProductType(e.target.value)}/>
+                                Tree
+                            <input
+                                type="radio"
+                                name="productType"
+                                value='litter-box'
+                                onChange={(e) => setProductType(e.target.value)}/>
+                                Litter Box
+                            <input
+                                type="radio"
+                                name="productType"
+                                value='accessory'
+                                onChange={(e) => setProductType(e.target.value)}/>
+                                Accessory
+                            <input
+                                type="radio"
+                                name="productType"
+                                value='tank'
+                                onChange={(e) => setProductType(e.target.value)}/>
+                                Tank
+                            <input
+                                type="radio"
+                                name="productType"
+                                value='heating-lighting'
+                                onChange={(e) => setProductType(e.target.value)}/>
+                                Heating/Lighting
+
+                    </div>
+                    <div className="form-div">
+                        <label className="form-label">In Stock</label>
+                    <br></br>
+                            <input
+                                type="radio"
+                                name="inStock"
+                                value={true}
+                                onChange={(e) => setInStock(e.target.true)}/>
+                                Yes
+                            <input
+                                type="radio"
+                                name="inStock"
+                                value={false}
+                                onChange={(e) => setInStock(e.target.false)}/>
+                                No
+                    </div>
+                    <div className="form-div">
+                        <label className="form-label">Is Popular</label>
+                    <br></br>
+                        <input
+                                type="radio"
+                                name="isPopular"
+                                value={true}
+                                onChange={(e) => setIsPopular(e.target.true)}/>
+                                Yes
+                            <input
+                                type="radio"
+                                name="isPopular"
+                                value={false}
+                                onChange={(e) => setIsPopular(e.target.false)}/>
+                                No
+
+                    </div>
+                    <div className="form-div">
+                        <label className="form-label">Image URL</label>
+                    <br></br>
+                        <input
+                            className="product-imgUrl-box"
+                            type="text"
+                            initialValue={filteredProduct.imgUrl}
+                            value={imgUrl}
+                            placeholder="Image URL"
+                            onChange={(e) => setImgUrl(e.target.value)}
+                            required />
+                        </div>
+                        <button className="" >Update Product</button>
+                    </form>
+            ))}    </div> : null }
 <br></br>
             {userId === 1 ? null :
             <form onSubmit={submitReview}>
